a81f37cd139ba06150256054bd48f7c2d43d5fa1
cleonissouza <cleonis-cls@outlook.com>
2026-04-29 23:50:38 +0100

    feat: implement Profile screen with baby info card and settings section using compose

--- app/src/main/java/com/example/componentetelaperfil/MainActivity.kt
+++ app/src/main/java/com/example/componentetelaperfil/MainActivity.kt
@@ -4,13 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.componentetelaperfil.ui.theme.ComponenteTelaPerfilTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +54,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComponenteTelaPerfilTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   ProfileScreen(innerPadding)
                 }
             }
         }
@@ -31,17 +62,222 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ProfileScreen(innerPadding1: PaddingValues) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Color(0xFFF8F8F8)
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(20.dp)
+        ) {
+            Text(
+                text = "Profile",
+                fontSize = 34.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "Your baby's information",
+                fontSize = 16.sp,
+                color = Color.Gray
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            BabyProfileCard()
+
+            Spacer(modifier = Modifier.height(55.dp))
+
+            Text(
+                text = "Settings",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Medium
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            SettingsCard()
+        }
+    }
+}
+
+@Composable
+fun BabyProfileCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(230.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF2D4D94),
+                            Color(0xFF9FA9C5)
+                        )
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Baby photo",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(RoundedCornerShape(28.dp)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(
+                    text = "Baby Name",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White
+                )
+
+                Text(
+                    text = "Some info",
+                    fontSize = 15.sp,
+                    color = Color.White.copy(alpha = 0.8f)
+                )
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    BabyInfoItem("12", "Weeks")
+                    BabyInfoItem("5kg", "Weight")
+                    BabyInfoItem("60cm", "Height")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BabyInfoItem(
+    value: String,
+    label: String
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = value,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+
+        Text(
+            text = label,
+            fontSize = 14.sp,
+            color = Color.White.copy(alpha = 0.8f)
+        )
+    }
+}
+
+@Composable
+fun SettingsCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFC1C4EE)
+        )
+    ) {
+        Column {
+            SettingsItem(
+                icon = Icons.Default.AccountBox,
+                title = "Account"
+            )
+
+            Divider(color = Color.LightGray)
+
+            SettingsItem(
+                icon = Icons.Default.Notifications,
+                title = "Notifications"
+            )
+
+            Divider(color = Color.LightGray)
+
+            SettingsItem(
+                icon = Icons.Default.Lock,
+                title = "Privacy"
+            )
+
+            Divider(color = Color.LightGray)
+
+            SettingsItem(
+                icon = Icons.Default.Info,
+                title = "Help"
+            )
+        }
+    }
+}
+
+@Composable
+fun SettingsItem(
+    icon: ImageVector,
+    title: String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Icon(
+            imageVector = icon,
+            contentDescription = title,
+            tint = Color.DarkGray,
+            modifier = Modifier.size(28.dp)
+        )
+
+        Spacer(modifier = Modifier.width(24.dp))
+
+        Text(
+            text = title,
+            fontSize = 18.sp,
+            color = Color.DarkGray,
+            modifier = Modifier.weight(1f)
+        )
+
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+            contentDescription = "Open $title",
+            tint = Color.DarkGray,
+            modifier = Modifier.size(32.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComponenteTelaPerfilTheme {
-        Greeting("Android")
+        ProfileScreen(
+            innerPadding1 = PaddingValues(0.dp)
+        )
     }
 }
